5c6bd8075b620d77606e30da18ed6cbfc1a60002
knakum39 <krishna.nakum39@gmail.com>
2026-06-13 21:25:46 +0530

    End to End Web Automation: Place order from ecommerce website

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,20 +1,42 @@
-const { test, expect } = require('@playwright/test');
- 
+const {test,expect, browser} = require('@playwright/test');
+//import {test,expect} from "@playwright/test"; TS
 
-test('@Web Client App login', async ({ page }) => {
-   //js file- Login js, DashboardPage
-   const email = "anshika@gmail.com";
-   const productName = 'zara coat 3';
-   const products = page.locator(".card-body");
+test('E2E add to card flow', async ({browser,})=>{
+
+   const uemail = "anshika@gmail.com";
+   const productName = "ZARA COAT 3";   
+
+   const context = await browser.newContext();
+   const page = await context.newPage();
+    const products = page.locator('.card-body');
+
    await page.goto("https://rahulshettyacademy.com/client");
-   await page.locator("#userEmail").fill(email);
-   await page.locator("#userPassword").type("Iamking@000");
-   await page.locator("[value='Login']").click();
-   await page.waitForLoadState('networkidle');
+
+   await page.locator("#userEmail").fill(uemail);
+   await page.locator("#userPassword").fill("Iamking@000");
+   await page.locator('#login').click();
+   //await page.locator('[name="login"]').click();
+
+   await page.waitForLoadState("networkidle")
+   //or
    await page.locator(".card-body b").first().waitFor();
-   const titles = await page.locator(".card-body b").allTextContents();
-   console.log(titles); 
- 
-})
 
+   const title = await products.allTextContents();
+   //JS is case sensitive language so use the camelcasing only...
+   console.log(title);
+   const count = await products.count();
 
+   //await page.waitForTimeout(4000);
+
+  for(let i=0; i < count; i++)
+   {
+      if(await products.nth(i).locator('b').textContent() == productName)
+         {
+//Write logic to add to cart
+            await products.nth(i).locator("text=Add To Cart").click();
+            break;
+      }
+   }
+await page.waitForTimeout(3000);
+
+});
